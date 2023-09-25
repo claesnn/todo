012aeb4e86084df3fd74846e81dd662b7509c4cd
Claes Nymand Nilsson <claes89@gmail.com>
2023-09-25 23:53:49 +0200

    Removed reducer and context

--- src/types/types.ts
+++ src/types/types.ts
@@ -3,18 +3,3 @@ export type Todo = {
   title: string;
   completed: boolean;
 };
-
-export interface State {
-  todos: Todo[];
-}
-
-export type Action =
-  | { type: "ADD_TODO"; payload: Todo }
-  | { type: "TOGGLE_TODO"; payload: number }
-  | { type: "DELETE_TODO"; payload: number };
-
-
-export type StateContext = {
-  state: State;
-  dispatch: React.Dispatch<Action>;
-};

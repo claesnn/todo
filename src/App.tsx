f3484d638c70a7e5c16935305b714ef1c9d8a8de
Claes Nymand Nilsson <claes89@gmail.com>
2023-09-26 22:22:24 +0200

    Added zod + react-hook-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,23 @@
-import { useState } from 'react'
-import { Button } from '@/components/ui/button'
-import { TodoTable } from './components/TodoTable'
-import { useTodos } from './store/useTodos'
-import { AddTodo } from './components/AddTodo'
+import { TodoTable } from "./components/TodoTable";
+import { useTodos } from "./store/useTodos";
+import { AddTodo } from "./components/AddTodo";
 
 function App() {
-  const [count, setCount] = useState(0)
-  const { todos, addTodo, toggleTodo, deleteTodo } = useTodos()
-  
+  const { todos, addTodo, toggleTodo, deleteTodo } = useTodos();
 
   return (
-    <div className='m-5'>
+    <div className="m-5">
       <div className="card">
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
         <AddTodo addTodo={addTodo} />
-        <TodoTable todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+        <div className="mt-5" />
+        <TodoTable
+          todos={todos}
+          toggleTodo={toggleTodo}
+          deleteTodo={deleteTodo}
+        />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite! and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,40 +1,58 @@
-import { useState } from "react"
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
-// TODO: Use Shadcn-UI to create a form to add a todo
+// TODO: Make it possible to submit on enter
+
+const formSchema = z.object({
+  title: z.string().min(3).max(20),
+});
 
 type Props = {
-    addTodo: (title: string) => void
+  addTodo: (title: string) => void;
+};
+
+export function AddTodo({ addTodo }: Props) {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof formSchema>) {
+    addTodo(data.title);
+    form.reset();
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input placeholder="Title" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Add Todo</Button>
+      </form>
+    </Form>
+  );
 }
-
-export function AddTodo({addTodo}: Props) {
-
-    const [title, setTitle] = useState('')
-    return (
-        <div className="card">
-        <div className="card-header">
-            <h3 className="card-title">Add Todo</h3>
-        </div>
-        <div className="card-body">
-            <div className="form-group">
-            <label htmlFor="todo-title">Title</label>
-            <input
-                id="todo-title"
-                type="text"
-                className="form-control"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-            />
-            </div>
-            <button
-            className="btn btn-primary"
-            onClick={() => {
-                addTodo && addTodo(title)
-                setTitle('')
-            }}
-            >
-            Add Todo
-            </button>
-        </div>
-        </div>
-    )
-    }
